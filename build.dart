71346c8a5d76a331f2021111e9b76e1dfc213f35
Kornel Maczyński <kornel661@gmail.com>
2015-03-23 15:09:39 +0000

    get rid of old dart pub files

--- build.dart
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2014, Kornel Maczyński. All rights reserved. Use of this source code
-// is governed by a license that can be found in the LICENSE file.
-
-// This file is only used by Dart Editor. It displays errors and warnings after
-// analyzing a polymer.dart app.
-
-export 'package:polymer/default_build.dart';
