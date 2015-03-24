5f9508bdaf3b93a48f5a64845d4a34c810f13252
Kornel Maczyński <kornel661@gmail.com>
2015-03-24 12:57:38 +0000

    guard against malformed addresses

--- web/main.dart
+++ web/main.dart
@@ -2,9 +2,38 @@
 // code is governed by AGPLv3 license that can be found in the LICENSE file.
 // If you want to contribute read the rules in the CONTRIBUTING file.
 
+import 'dart:html';
 import 'package:polymer/polymer.dart';
 
-main() => initPolymer();
+bool _verifyUrl(String url) {
+  try {
+    Uri.parse(url);
+  } catch (_) {
+    return false;
+  }
+  return true;
+}
+
+main() {
+  // try to guard against malformed urls
+  String url = window.location.href;
+  if (!_verifyUrl(url)) {
+    int i = url.indexOf('#');
+    if (i > 0) {
+      url = url.substring(0, i);
+      window.location.href = url;
+      if (!_verifyUrl(url)) {
+        document.title = 'Trikipedia: malformed address.';
+        return;
+      }
+    } else {
+      document.title = 'Trikipedia: malformed address.';
+      return;
+    }
+  }
+  // start polymer
+  initPolymer();
+}
 
 @initMethod
 void realMain() {
